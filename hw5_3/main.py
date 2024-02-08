e2a581cf1972bfaa7fda7efe3e8048626a0eb5d1
Anton Asieiev <ntn.kiev@gmail.com>
2024-02-08 17:19:50 +0200

    5_3

--- hw5_3/main.py
+++ hw5_3/main.py
@@ -1,7 +1,16 @@
-
 from collections import defaultdict
+import argparse
 
-default_path = "hw5_3\\logfile.log"
+parser = argparse.ArgumentParser(description='Logfile parcer')
+parser.add_argument("filename", help="Logfile path")
+parser.add_argument("-info", required=False, default=False, help="Display INFO records", action="store_true")
+parser.add_argument("-debug", required=False, default=False, help="Display DEBUG records", action="store_true")
+parser.add_argument("-error", required=False, default=False, help="Display ERROR records", action="store_true")
+parser.add_argument("-warning", required=False, default=False, help="Display WARNING records", action="store_true")
+
+args = parser.parse_args()
+
+# default_path = "hw5_3\\logfile.log"
 
 def load_logs(file_path: str) -> list:
 
@@ -27,7 +36,7 @@ def load_logs(file_path: str) -> list:
 def filter_logs_by_level(logs: list, level: str) -> list:
     level_list = []
     for item in logs:
-        if item["level"] == level:
+        if item["level"].lower() == level:
             level_list.append(item)
     return level_list
 
@@ -42,22 +51,36 @@ def display_log_counts(counts: dict):
     column2 = "| Кількість"
     width_col1 = len(column1)
     width_col2 = len(column2)
-    print(column1 + column2)
-    print(f"-" * len(column1) + "|" + "-" * len(column2))
+    print("\n" + column1 + column2)
+    print("-" * width_col1 + "|" + "-" * width_col2)
     for key, value in counts.items():
-        print(f"{key:<width_col1}|{value:<width_col2}")
-
-
-    pass
-
+        print(f"{key:<{width_col1}}| {value:<{width_col2}}")
+    print()
+    
 def main():
-    log = load_logs(default_path)
-    # print(log)
-    # log_level = filter_logs_by_level(log, "ERROR")
-    print(count_logs_by_level(log))
-    counts = count_logs_by_level(log)
+    def print_debug(list_logs):
+        for rec in list_logs:
+            print(rec["date"], end = " ")
+            print(rec["time"], end = " - ")
+            print(rec["description"], end = "")
+
+    logs = load_logs(args.filename)
+    print(logs)
+    counts = count_logs_by_level(logs)
     display_log_counts(counts)
-
-
+    if args.info:
+        print(f"Деталі логів для рівня INFO:")
+        print_debug(filter_logs_by_level(logs, "info"))
+    if args.debug:
+        print(f"Деталі логів для рівня DEBUG:")
+        print_debug(filter_logs_by_level(logs, "debug"))
+    if args.error:
+        print(f"Деталі логів для рівня ERROR:")
+        print_debug(filter_logs_by_level(logs, "error"))
+    if args.warning:
+        print(f"Деталі логів для рівня WARNING:")
+        print_debug(filter_logs_by_level(logs, "warning"))
+    
+    
 if __name__ == "__main__":
     main()
